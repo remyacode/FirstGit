82f6d2df988c74759015b5c81700c6fccd4fc5ed
remyacode <remyachandrasekhar710@gmail.com>
2023-04-30 22:07:35 +0530

    task 9

--- Task 1-7/DOM/main.js
+++ Task 1-7/DOM/main.js
@@ -2,6 +2,8 @@ var form=document.getElementById('addForm');
 var itemList=document.getElementById('items');
 var filter=document.getElementById('filter');
 
+
+
 //form submit event
 form.addEventListener('submit',addItem);
 
@@ -14,6 +16,45 @@ itemList.addEventListener('click',removeItem);
 //filter event
 filter.addEventListener('keyup',filteritems);
 
+//Description
+/*
+<form id="addForm" class="form-inline mb-3">
+      <input type="text" class="form-control mr-2" id="item">
+      <input type="submit" class="btn btn-dark" value="Submit">
+
+      //
+*/
+      //create description on existing html
+
+var newdiv=document.createElement('input');
+
+newdiv.type='text';
+
+//add class
+newdiv.className='form-control mr-2';
+
+//add id
+newdiv.id='description';
+
+//add attribute
+newdiv.setAttribute('title','hello div');
+
+//create text node
+var newd=document.createTextNode('Hello World');
+//add text todvi
+newdiv.appendChild(newd);
+
+
+var contain=document.querySelector('.container #main #addForm');
+var h1=document.querySelector('.container #addForm #item');
+
+
+//console.log(newdiv);
+
+//newdiv.style.fontSize='30px';
+
+h1.insertAdjacentElement("afterend",newdiv);
+
 
 
 
@@ -23,15 +64,17 @@ function addItem(e){
 
     //get input value
     var newitem=document.getElementById('item').value;
+    var newdes=document.getElementById('description').value;
 
     //create new li element
     var li=document.createElement('li');
     //add class
     li.className='list-group-item';
-    console.log(li);
+    //console.log(li);
     //add text node with input value
     li.appendChild(document.createTextNode(newitem));
-
+    li.appendChild(document.createElement('br'));
+    li.appendChild(document.createTextNode(newdes));
     //create delete button
     var deleteBtn = document.createElement('button');
 
@@ -50,16 +93,18 @@ function addItem(e){
     deleteBtn.appendChild(document.createTextNode('X'));
 
     //append text node edit
-    editBtn.appendChild(document.createTextNode('edit'));
+    editBtn.appendChild(document.createTextNode('EDIT'));
 
 //remya: to order the buttons in list
 
-//append edit to li
-li.appendChild(editBtn);
+
 
     
     //append button to li
     li.appendChild(deleteBtn);
+
+    //append edit to li
+li.appendChild(editBtn);
 
     
 
@@ -93,12 +138,17 @@ function filteritems(e){
     //convert to array
     Array.from(items).forEach(function(item){
         var itemName=item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text)!=-1){
+        var dname=item.childNodes[2].textContent;
+        
+        
+        if(itemName.toLowerCase().indexOf(text)!=-1 || dname.toLowerCase().indexOf(text)!=-1){
             item.style.display='block';
         }else{
             item.style.display='none';
+          
         }
     });
+
 }
 
 
