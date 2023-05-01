70db10187f9f97260e680d7debf6f761e6e1aa56
remyacode <remyachandrasekhar710@gmail.com>
2023-05-01 11:29:28 +0530

    task 10

---
+++ Task 10/main.js
@@ -0,0 +1,53 @@
+
+//const ul=document.querySelector('.items');
+//ul.firstElementChild.textContent='HELLO';
+//ul.children[0].style.color='green';
+//ul.children[1].style.color='yellow';
+
+const btn=document.querySelector('.btn');
+btn.addEventListener('click',(e1)=>{
+    e1.preventDefault();
+    console.log('click');
+});
+btn.addEventListener('mouseover',(e1)=>{
+    
+    console.log('mouseover');
+});
+btn.addEventListener('mouseout',(e1)=>{
+    
+    console.log('mouseout');
+});
+
+const myForm=document.querySelector('my-form');
+const nameInput=document.querySelector('#name');
+const emailInput=document.querySelector('#email');
+const msg=document.querySelector('.msg');
+const userList=document.querySelector('#users');
+
+btn.addEventListener('click',onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value===''||emailInput.value===''){
+        //alert('Please enetr fields');
+        msg.classList.add('error');
+        msg.innerHTML='Please enter all fields';
+        setTimeout(()=>msg.remove(),3000);
+    }
+    else{
+        const li=document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
+        localStorage.setItem('Name',nameInput.value);
+        console.log(localStorage.getItem('Name'));
+        localStorage.setItem('Email',emailInput.value);
+        console.log(localStorage.getItem('Email'));
+        //localStorage.setItem(nameInput.value,emailInput.value);
+        //console.log(localStorage.getItem(nameInput.value));
+        userList.appendChild(li);
+
+        //Clear
+        nameInput.value='';
+        emailInput.value='';
+    }
+}
