a09a5eece8d6667ef69cfe68d40e0fc49d6a1cb2
remyacode <remyachandrasekhar710@gmail.com>
2023-05-01 14:40:59 +0530

    task 13

--- Task 10/main.js
+++ Task 10/main.js
@@ -4,7 +4,7 @@
 //ul.children[0].style.color='green';
 //ul.children[1].style.color='yellow';
 
-const btn=document.querySelector('.btn');
+const btn=document.querySelector('#my-form .btn');
 btn.addEventListener('click',(e1)=>{
     e1.preventDefault();
     console.log('click');
@@ -17,6 +17,8 @@ btn.addEventListener('mouseout',(e1)=>{
     
     console.log('mouseout');
 });
+
+
 
 const myForm=document.querySelector('my-form');
 const nameInput=document.querySelector('#name');
@@ -37,7 +39,25 @@ function onSubmit(e){
     }
     else{
         const li=document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
+        li.id='listitem';
+        
+        ///////////delete
+        
+        const delBtn=document.createElement('button');
+        
+        //add classes to delete button
+  delBtn.className='btn btn-danger btn-sm float-right delete';
+  delBtn.id='del';
+  //append text node delete
+  delBtn.appendChild(document.createTextNode('Delete'));
+
+
+  li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
+
+
+      //append button to li
+      li.appendChild(delBtn);
+
         //localStorage.setItem('Name',nameInput.value);
         //console.log(localStorage.getItem('Name'));
         //localStorage.setItem('Email',emailInput.value);
@@ -45,26 +65,71 @@ function onSubmit(e){
         //localStorage.setItem(nameInput.value,emailInput.value);
         //console.log(localStorage.getItem(nameInput.value));
 
+///////////////
+
+
+
 
         /////LOCAL STORAGE AS OBJECT
 
-        let ob={
+     const ob={
             "Name":nameInput.value,
             "Email":emailInput.value
         };
 
-        let obs=JSON.stringify(ob);
+        var obs=JSON.stringify(ob);
         localStorage.setItem(emailInput.value,obs);
         userList.appendChild(li);
-        
+        //console.log(obs);
 
         //getting deserialised
      
         let obd=JSON.parse(localStorage.getItem(emailInput.value));
-        console.log(obd);
+        //console.log(obd);
+        //delBtn.addEventListener('click',removeItem);
 
+        ////
+        delBtn.onclick=(e)=>{
+            localStorage.removeItem(ob.Email);
+            
+            ////////
+            var itemList=document.getElementById('listitem').parentNode;
+        if(e.target.classList.contains('delete')){
+        //if(confirm('Are you Sure?')){
+            var li=e.target.parentElement;
+            
+            //localStorage.removeItem(ob.Email);
+            itemList.removeChild(li);
+            
+        //}
+    }
+            /////////
+
+
+            }
+
+        }
         //Clear
         nameInput.value='';
         emailInput.value='';
     }
+
+
+/*
+
+
+//remove item
+function removeItem(e){
+    var itemList=document.getElementById('listitem').parentNode;
+    if(e.target.classList.contains('delete')){
+        //if(confirm('Are you Sure?')){
+            var li=e.target.parentElement;
+            
+            //localStorage.removeItem(ob.Email);
+            itemList.removeChild(li);
+            //https://www.youtube.com/embed/5yIm_UgaCYI
+        //}
+    }
 }
+
+*/
